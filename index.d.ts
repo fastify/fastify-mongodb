0c0c8b6c63aca8d58f0c236988905f4f322e2d6c
Shubham Parihar <shubhamparihar391@gmail.com>
2021-08-06 19:37:39 +0530

    refactor(index.d.ts): export all interfaces and some clean up

--- index.d.ts
+++ index.d.ts
@@ -1,54 +1,52 @@
-import * as mongodb from 'mongodb';
-import fastify, { FastifyPlugin } from 'fastify';
+import type { FastifyPlugin } from 'fastify';
+import type { Db, MongoClient, ObjectId } from 'mongodb';
 
-declare namespace fastifyMongodb {
-  interface FastifyMongoObject {
-    /**
-     * Mongo client instance
-     */
-    client: mongodb.MongoClient;
-    /**
-     * DB instance
-     */
-    db?: mongodb.Db;
-    /**
-     * Mongo ObjectId class
-     */
-    ObjectId: typeof mongodb.ObjectId;
-  }
+export interface FastifyMongoObject {
+  /**
+   * Mongo client instance
+   */
+  client: MongoClient;
+  /**
+   * DB instance
+   */
+  db?: Db;
+  /**
+   * Mongo ObjectId class
+   */
+  ObjectId: typeof ObjectId;
+}
 
-  interface FastifyMongoNestedObject {
-    [name: string]: FastifyMongoObject;
-  }
+export interface FastifyMongoNestedObject {
+  [name: string]: FastifyMongoObject;
+}
 
-  interface FastifyMongodbOptions {
-    /**
-     * Force to close the mongodb connection when app stopped
-     * @default false
-     */
-    forceClose?: boolean;
-    /**
-     * Database name to connect
-     */
-    database?: string;
-    name?: string;
-    /**
-     * Pre-configured instance of MongoClient
-     */
-    client?: mongodb.MongoClient;
-    /**
-     * Connection url
-     */
-    url?: string;
-  }
+export interface FastifyMongodbOptions {
+  /**
+   * Force to close the mongodb connection when app stopped
+   * @default false
+   */
+  forceClose?: boolean;
+  /**
+   * Database name to connect
+   */
+  database?: string;
+  name?: string;
+  /**
+   * Pre-configured instance of MongoClient
+   */
+  client?: MongoClient;
+  /**
+   * Connection url
+   */
+  url?: string;
 }
 
 declare module 'fastify' {
   interface FastifyInstance {
-    mongo: fastifyMongodb.FastifyMongoObject & fastifyMongodb.FastifyMongoNestedObject;
+    mongo: FastifyMongoObject & FastifyMongoNestedObject;
   }
 }
 
-declare const fastifyMongodb: FastifyPlugin<fastifyMongodb.FastifyMongodbOptions>;
+export const fastifyMongodb: FastifyPlugin<FastifyMongodbOptions>;
 
 export default fastifyMongodb;
