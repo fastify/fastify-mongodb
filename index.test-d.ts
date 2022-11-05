ece4358509deb7277b25086e0de0dc08966fc440
Frazer Smith <frazer.dev@outlook.com>
2022-11-05 09:06:31 +0000

    test(types): remove unused variable assignments (#195)

--- index.test-d.ts
+++ index.test-d.ts
@@ -10,9 +10,9 @@ app
     url: 'mongodb://localhost:27017/testdb',
   })
   .after((err) => {
-    const dbTest = app.mongo.client.db('test');
-    const dbDb = app.mongo.db!;
+    app.mongo.client.db('test');
+    app.mongo.db!;
     const ObjectId = app.mongo.ObjectId;
-    const myId = new ObjectId('aaaa');
-    const myId2 = new reExportedObjectId('aaaa');
+    new ObjectId('aaaa');
+    new reExportedObjectId('aaaa');
   });
