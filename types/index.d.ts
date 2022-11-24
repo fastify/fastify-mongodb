a3315d388a3be6e6d9b1cbfdf6b0391a14509b5b
Uzlopak <aras.abbasi@googlemail.com>
2022-11-24 08:57:06 +0100

    Restructure + nodenext compatibility (#197)
    * restructure
    
    * nodenext compatibility
    
    * disable package-lock generation
    
    * fix

---
+++ types/index.d.ts
@@ -0,0 +1,62 @@
+import type { FastifyPluginAsync } from 'fastify';
+import type { Db, MongoClient, MongoClientOptions } from 'mongodb';
+import { ObjectId } from 'mongodb';
+
+declare module 'fastify' {
+  interface FastifyInstance {
+    mongo: fastifyMongodb.FastifyMongoObject & fastifyMongodb.FastifyMongoNestedObject;
+  }
+}
+
+type FastifyMongodb = FastifyPluginAsync<fastifyMongodb.FastifyMongodbOptions>;
+
+declare namespace fastifyMongodb {
+
+  export interface FastifyMongoObject {
+    /**
+     * Mongo client instance
+     */
+    client: MongoClient;
+    /**
+     * DB instance
+     */
+    db?: Db;
+    /**
+     * Mongo ObjectId class
+     */
+    ObjectId: typeof ObjectId;
+  }
+
+  export interface FastifyMongoNestedObject {
+    [name: string]: FastifyMongoObject;
+  }
+
+  export interface FastifyMongodbOptions extends MongoClientOptions {
+    /**
+     * Force to close the mongodb connection when app stopped
+     * @default false
+     */
+    forceClose?: boolean;
+    /**
+     * Database name to connect
+     */
+    database?: string;
+    name?: string;
+    /**
+     * Pre-configured instance of MongoClient
+     */
+    client?: MongoClient;
+    /**
+     * Connection url
+     */
+    url?: string;
+  }
+
+  export { ObjectId }
+
+  export const fastifyMongodb: FastifyMongodb
+  export { fastifyMongodb as default }
+}
+
+declare function fastifyMongodb(...params: Parameters<FastifyMongodb>): ReturnType<FastifyMongodb>
+export = fastifyMongodb
